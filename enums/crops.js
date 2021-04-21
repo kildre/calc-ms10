c52702f470dfcb62d9b2a744d18ac741c764cc0c
Andy Erickson <aerickson@modernclimate.com>
2021-04-21 09:14:51 -0500

    Adding a crop drop-down

--- enums/crops.js
+++ enums/crops.js
@@ -1,6 +1,14 @@
 export const CROPS = [
-  Corn: {
-    calculators: ['S10'],
-    costPerBushel: 3.50
+  {
+    name: "Corn",
+    calculators: ["S10"],
+    currentPrice: 3.0,
+    yieldAdvantage: 3.0
+  },
+  {
+    name: "Soybean",
+    calculators: ["S10"],
+    currentPrice: 4.23,
+    yieldAdvantage: 2.43
   }
 ];
